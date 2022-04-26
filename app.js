66d221abc77b617122b29b7eb3ead8d0f959febd
Rabbani222 <gmgolamrabbani9@gmail.com>
2022-04-26 23:53:09 +0600

    all files upload done

---
+++ app.js
@@ -0,0 +1,454 @@
+const students_list = [
+
+    {
+
+        Name: 'Fahim',
+        Roll: '1',
+        Age: '15',
+        Class_number: '7',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '8000'
+
+    },
+
+    {
+
+        Name: 'Fatema',
+        Roll: '2',
+        Age: '25',
+        Class_number: '8',
+        Gender: 'Female',
+        Location: 'Dhaka',
+        Admission_Fees: '5000'
+    },
+
+    {
+
+        Name: 'Titu',
+        Roll: '3',
+        Age: '12',
+        Class_number: '5',
+        Gender: 'Male',
+        Location: 'Kuakata',
+        Admission_Fees: '3000'
+    },
+
+    {
+
+        Name: 'Safa',
+        Roll: '4',
+        Age: '15',
+        Class_number: '4',
+        Gender: 'Female',
+        Location: 'Mirpur',
+        Admission_Fees: '2000'
+    },
+
+    {
+        Name: 'Bahaddin',
+        Roll: '5',
+        Age: '35',
+        Class_number: '3',
+        Gender: 'Male',
+        Location: 'Madaripur',
+        Admission_Fees: '5000'
+    },
+
+    {
+
+        Name: 'Kabir',
+        Roll: '6',
+        Age: '45',
+        Class_number: '6',
+        Gender: 'Male',
+        Location: 'Bhola',
+        Admission_Fees: '6000',
+    },
+
+    {
+
+        Name: 'Anika',
+        Roll: '7',
+        Age: '45',
+        Class_number: '9',
+        Gender: 'Female',
+        Location: 'Kolapara',
+        Admission_Fees: '1500',
+    },
+
+    {
+
+        Name: 'Piyash',
+        Roll: '8',
+        Age: '10',
+        Class_number: '8',
+        Gender: 'Male',
+        Location: 'Uttora',
+        Admission_Fees: '1800',
+    },
+
+    {
+
+        Name: 'Piyash',
+        Roll: '8',
+        Age: '10',
+        Class_number: '8',
+        Gender: 'Male',
+        Location: 'Uttora',
+        Admission_Fees: '1800',
+    },
+
+    {
+
+        Name: 'Sowrov',
+        Roll: '9',
+        Age: '5',
+        Class_number: '6',
+        Gender: 'Male',
+        Location: 'Badda',
+        Admission_Fees: '2500',
+    },
+
+    {
+
+        Name: 'Hasan',
+        Roll: '10',
+        Age: '6',
+        Class_number: '5',
+        Gender: 'Male',
+        Location: 'Amtoli',
+        Admission_Fees: '3500',
+    },
+
+    {
+
+        Name: 'Hasan',
+        Roll: '11',
+        Age: '9',
+        Class_number: '3',
+        Gender: 'Female',
+        Location: 'Lebukhali',
+        Admission_Fees: '4500',
+    },
+
+    {
+
+        Name: 'Hosen',
+        Roll: '12',
+        Age: '12',
+        Class_number: '4',
+        Gender: 'Female',
+        Location: 'Patuakhali',
+        Admission_Fees: '4700',
+    },
+
+    {
+
+        Name: 'Piyal',
+        Roll: '13',
+        Age: '14',
+        Class_number: '9',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '3700',
+    },
+
+    {
+
+        Name: 'Bushra',
+        Roll: '14',
+        Age: '18',
+        Class_number: '2',
+        Gender: 'Female',
+        Location: 'Dumki',
+        Admission_Fees: '4700',
+    },
+
+    {
+
+        Name: 'Kabila',
+        Roll: '15',
+        Age: '11',
+        Class_number: '6',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '6700',
+    },
+
+    {
+
+        Name: 'Mithu',
+        Roll: '16',
+        Age: '19',
+        Class_number: '5',
+        Gender: 'Male',
+        Location: 'Dhaka',
+        Admission_Fees: '7500',
+    },
+
+    {
+
+        Name: 'Maya',
+        Roll: '17',
+        Age: '20',
+        Class_number: '7',
+        Gender: 'Female',
+        Location: 'Mirpur',
+        Admission_Fees: '9200',
+    },
+
+    {
+
+        Name: 'Nahid',
+        Roll: '18',
+        Age: '23',
+        Class_number: '4',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '8300',
+    },
+
+    {
+
+        Name: 'Sufia',
+        Roll: '19',
+        Age: '26',
+        Class_number: '3',
+        Gender: 'Female',
+        Location: 'Kolapara',
+        Admission_Fees: '10000',
+    },
+
+    {
+
+        Name: 'Sumon',
+        Roll: '20',
+        Age: '32',
+        Class_number: '5',
+        Gender: 'Male',
+        Location: 'Amtoli',
+        Admission_Fees: '11000',
+    },
+
+    {
+
+        Name: 'Uttom',
+        Roll: '21',
+        Age: '33',
+        Class_number: '10',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '12000',
+    },
+
+    {
+
+        Name: 'Korim',
+        Roll: '22',
+        Age: '38',
+        Class_number: '8',
+        Gender: 'Male',
+        Location: 'Golachipa',
+        Admission_Fees: '13000',
+    },
+
+    {
+
+        Name: 'Sakil',
+        Roll: '23',
+        Age: '39',
+        Class_number: '10',
+        Gender: 'Male',
+        Location: 'Amkhola',
+        Admission_Fees: '13500',
+    },
+
+    {
+
+        Name: 'Aysha',
+        Roll: '24',
+        Age: '36',
+        Class_number: '8',
+        Gender: 'Female',
+        Location: 'Badura',
+        Admission_Fees: '14500',
+    },
+
+    {
+
+        Name: 'Fardin',
+        Roll: '25',
+        Age: '7',
+        Class_number: '4',
+        Gender: 'Male',
+        Location: 'Golachipa',
+        Admission_Fees: '5500',
+    },
+
+    {
+
+        Name: 'Kalam',
+        Roll: '26',
+        Age: '8',
+        Class_number: '7',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '3300',
+    },
+
+    {
+
+        Name: 'Jamila',
+        Roll: '27',
+        Age: '18',
+        Class_number: '5',
+        Gender: 'Female',
+        Location: 'Zhalokhathi',
+        Admission_Fees: '2500',
+    },
+
+    {
+
+        Name: 'Mamun',
+        Roll: '28',
+        Age: '20',
+        Class_number: '6',
+        Gender: 'Male',
+        Location: 'Patuakhali',
+        Admission_Fees: '1200',
+    },
+
+    {
+
+        Name: 'Debu',
+        Roll: '29',
+        Age: '19',
+        Class_number: '9',
+        Gender: 'Male',
+        Location: 'Dumki',
+        Admission_Fees: '9300',
+    },
+
+    {
+
+        Name: 'Kamila',
+        Roll: '30',
+        Age: '20',
+        Class_number: '10',
+        Gender: 'Female',
+        Location: 'Zhalokhathi',
+        Admission_Fees: '10000',
+    },
+];
+
+
+
+
+// students_list.forEach((data, index) => {
+//     console.log(data);
+// });
+
+
+
+
+
+// - Total Admnission fees
+
+// let total_amount = 0;
+
+// students_list.forEach((data, index) => {
+//     console.log(` 
+//     Name : ${data.Name} 
+//     Admission_fees : ${data.Admission_Fees} `);
+//     total_amount += +data.Admission_Fees
+// });
+
+// console.log(`
+
+// ===============================>
+// Total_Amount = ${total_amount} Taka.
+
+
+// `);
+
+
+
+
+// - Find All Female Students
+
+// students_list.forEach((data, index) => {
+//     if (data.Gender == 'Female') {
+//         console.log(` 
+//         Name : ${data.Name} 
+//         Gender : ${data.Gender}
+//          `);
+//     }
+// });
+
+
+// - Find class wise student result
+
+// students_list.forEach((data, index) => {
+//     if (data.Class_number == '3') {
+//         console.log(` 
+
+//             Name : ${data.Name}
+//             Class_number : ${data.Class_number}
+
+//         `);
+//     }
+//     if (data.Class_number == '4') {
+//         console.log(` 
+
+//             Name : ${data.Name}
+//             Class_number : ${data.Class_number}
+
+//         `);
+//     }
+//     if (data.Class_number == '5') {
+//         console.log(` 
+
+//             Name : ${data.Name}
+//             Class_number : ${data.Class_number}
+
+//         `);
+//     }
+// });
+
+
+
+
+// - Location wise student result
+
+// students_list.forEach((data, index) => {
+//     if (data.Location == 'Patuakhali') {
+//         console.log(` 
+//             Name : ${data.Name}
+//             Location : ${data.Location}
+
+//         `);
+//     }
+// });
+
+
+// students_list.forEach((data, index) => {
+//     console.log(` 
+//         Name : ${data.Name}
+//         Location : ${data.Location}
+//     `);
+// });
+
+
+
+
+// - find student between 10 - 25 age
+
+// students_list.forEach((data, index) => {
+//     if (data.Age >= 10 && data.Age <= 25) {
+//         console.log(` 
+//             Name : ${data.Name}
+//             Age : ${data.Age}
+//         `);
+//     }
+// });
